a9a136f67d96f9dc884f099919868b7eaa304141
sachingavand <132877597+sachingavand@users.noreply.github.com>
2023-12-13 11:25:22 +0530

    Update Home.js

--- src/component/Home.js
+++ src/component/Home.js
@@ -2,7 +2,7 @@ import React, {Component, Fragment} from 'react';
 import Header from './Header';
 import RestaurantsDisplay from './Restaurants';
 
-const url = "https://developerfunnel.herokuapp.com/restaurants";
+const url = "http://10.0.14.207:8443/restaurant";
 
 class Home extends Component{
     constructor(){
